648945baa7b62cead7812f10a8a19335738b38a1
masud001 <madhnagar@gmail.com>
2020-11-17 03:14:37 +0600

    node added module

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const http = require("http");
+const tutorial = require("./tutorial");
 const hostName = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader("Content-Type", "text/plain");
-	res.end("Hello world! \n ");
+	res.end("Hello world! Masud \n ");
 });
 
 server.listen(port, hostName, () => {
 	console.log(`server running on ${hostName}: ${port}`);
 });
+
+console.log(tutorial.minus(10, 6));
+console.log(tutorial.sum(10, 5));
