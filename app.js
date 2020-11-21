1bc754f55139dcdd7cc94e3592312aee26cedd8d
masud001 <madhnagar@gmail.com>
2020-11-22 04:42:42 +0600

    eventemitter is started to learn

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+const { Socket } = require("dgram");
 const http = require("http");
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+	if (req.url === "/") {
+		res.write("hello world");
+		res.end();
+	}
+	if (req.url === "/masud/age") {
+		res.write(JSON.stringify([1, 3, 4, 5, 6, 7]));
+		res.end();
+	}
+});
 
 server.listen(3000);
-
 console.log("listening on port 3000....");
