b388f42032f4d9cc783aa2c60a3da5af03afe206
masud001 <madhnagar@gmail.com>
2020-11-22 02:21:44 +0600

    os module on node is a fantustic build in module in node js....git add .!

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
-
+const files = fs.readdirSync("./");
+console.log("this is sync function : ", files);
 fs.readdir("./", function (err, result) {
 	if (err) {
-		console.log("Error ", err);
+		console.log("Error ta dekh :", err);
 	} else {
 		console.log("Result :", result);
 	}
