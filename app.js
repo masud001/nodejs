53b6fe332b0d2b740b720a6e6b5857ee9375a780
masud001 <madhnagar@gmail.com>
2020-11-22 02:16:49 +0600

    in a file system there are 2 way to declare... one is blocking and 2 is non-blocking

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-const http = require("http");
-const tutorial = require("./tutorial");
-const hostName = "127.0.0.1";
-const port = 3000;
+const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/plain");
-	res.end("Hello world! Masud \n ");
+fs.readdir("./", function (err, result) {
+	if (err) {
+		console.log("Error ", err);
+	} else {
+		console.log("Result :", result);
+	}
 });
-
-server.listen(port, hostName, () => {
-	console.log(`server running on ${hostName}: ${port}`);
-});
-
-console.log(tutorial.minus(10, 6));
-console.log(tutorial.sum(10, 5));
