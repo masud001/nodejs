497bba21a3aba5881695fb3cf30e670e30dafcac
masud001 <madhnagar@gmail.com>
2020-11-23 00:03:40 +0600

    building restful API using express

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
 	if (req.url === "/") {
-		res.write("hello world");
-		res.end();
+		// ...
 	}
 	if (req.url === "/masud/age") {
-		res.write(JSON.stringify([1, 3, 4, 5, 6, 7]));
-		res.end();
+		// ...
 	}
 });
 
