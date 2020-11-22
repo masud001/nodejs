abd5ca27ebfce0adb29fdb0406f18e011e6ca1c7
masud001 <madhnagar@gmail.com>
2020-11-23 01:31:28 +0600

    pass the parameter with given url

--- express-demo/index.js
+++ express-demo/index.js
@@ -8,8 +8,15 @@ app.get("/", (req, res) => {
 	res.send("Hello world");
 });
 
+// courses route
 app.get("/api/courses", (req, res) => {
 	res.send([1, 2, 3, 4]);
 });
 
-app.listen(PORT, () => console.log(`listening port on ${PORT}`));
+// route for single course with ID
+app.get("/api/courses/:id", (req, res) => {
+	res.send(req.params.id);
+});
+
+let port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`listening port on ${port}`));
