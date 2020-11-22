1a1b4e9afd4ef73b7b97444f7e007b1279d5b3f2
masud001 <madhnagar@gmail.com>
2020-11-23 04:35:29 +0600

    updated courses will start tomorrow

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -28,9 +29,19 @@ app.get("/api/courses/:id", (req, res) => {
 	res.send(course);
 });
 
-// post requests routes
+// post requests routes CREATE COURSES
 
 app.post("/api/courses", (req, res) => {
+	// Data velidate using Joi NPM
+	const schema = Joi.object({
+		name: Joi.string().min(3).required(),
+	});
+	const result = schema.validate(req.body);
+
+	if (result.error) {
+		res.status(400).send(result.error.details[0].message);
+		return;
+	}
 	const course = {
 		id: courses.length + 1,
 		name: req.body.name,
@@ -38,6 +49,8 @@ app.post("/api/courses", (req, res) => {
 	courses.push(course);
 	res.send(course);
 });
+
+// UPDATE COURSES with all routes
 
 let port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening port on ${port}`));
